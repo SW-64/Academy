8b86b0d529be18baf1d1fbabf32089c8426b3ff3
SW-64 <ettjddns6@gmail.com>
2025-11-10 16:15:19 +0900

    FEAT: DTO 검증을 위한 글로벌 파이프라인 설정

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 글로벌 파이프라인 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 없는 필드 자동 제거
+      forbidNonWhitelisted: false, // whitelist에서 제거되는 필드를 에러로 처리할지 말지 결정.
+      transform: true, // string → number 자동 변환
+    }),
+  );
   // Swagger 설정
   const config = new DocumentBuilder()
     .setTitle('API')
