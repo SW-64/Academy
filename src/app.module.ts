8830d187e3fd92800ecab740dc897b25800f512c
SW-64 <ettjddns6@gmail.com>
2025-11-10 14:55:49 +0900

    FEAT: 리소스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { StudentsModule } from './students/students.module';
+import { ParentsModule } from './parents/parents.module';
+import { AdminModule } from './admin/admin.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, StudentsModule, ParentsModule, AdminModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
