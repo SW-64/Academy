15238bf9e0c03cb50b0ffdada61ac69411e853ab
SW-64 <ettjddns6@gmail.com>
2025-11-10 16:13:32 +0900

    FEAT: env 검증 및 전역 사용 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,9 +7,22 @@ import { StudentsModule } from './students/students.module';
 import { ParentsModule } from './parents/parents.module';
 import { AdminModule } from './admin/admin.module';
 import { UsersModule } from './users/users.module';
+import { configModuleValidationSchema } from './configs/env-validation.config';
 
 @Module({
-  imports: [AuthModule, StudentsModule, ParentsModule, AdminModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validationSchema: configModuleValidationSchema,
+      envFilePath: '.env',
+    }),
+    AuthModule,
+    StudentsModule,
+    ParentsModule,
+    AdminModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
