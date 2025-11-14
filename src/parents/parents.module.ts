25aea731f5d697ef397ac73e3871fd8222503b1d
gil-hyeon <lgh5498@naver.com>
2025-11-14 16:26:06 +0900

    FEAT: Parents 모듈내에 Student 엔티티 등록 및 ParentsRepository 클래스 등록

--- src/parents/parents.module.ts
+++ src/parents/parents.module.ts
@@ -3,10 +3,12 @@ import { ParentsService } from './parents.service';
 import { ParentsController } from './parents.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Parent } from './entities/parent.entity';
+import { Student } from '../students/entities/student.entity';
+import { ParentsRepository } from './parents.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Parent])],
+  imports: [TypeOrmModule.forFeature([Parent, Student])],
   controllers: [ParentsController],
-  providers: [ParentsService],
+  providers: [ParentsService, ParentsRepository],
 })
 export class ParentsModule {}
