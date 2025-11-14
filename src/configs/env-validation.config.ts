0221510464d4c8d87cf20a20e42683cbc1d714b3
SW-64 <ettjddns6@gmail.com>
2025-11-14 18:33:24 +0900

    UPDATE: env값 업데이트

--- src/configs/env-validation.config.ts
+++ src/configs/env-validation.config.ts
@@ -21,4 +21,12 @@ export const configModuleValidationSchema = Joi.object({
     'any.required': 'DB_NAME is required',
     'string.empty': 'DB_NAME cannot be empty',
   }),
+  SERVER_PORT: Joi.number().port().required().messages({
+    'any.required': 'SERVER_PORT is required',
+    'number.base': 'SERVER_PORT must be a number',
+  }),
+  PASSWORD_HASH: Joi.number().integer().min(4).max(31).required().messages({
+    'any.required': 'PASSWORD_HASH is required',
+    'number.base': 'PASSWORD_HASH must be a number',
+  }),
 });
