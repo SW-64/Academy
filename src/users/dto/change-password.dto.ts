7d8333aa0711228ebe39bc8eac543c96a4fd4af1
SW-64 <ettjddns6@gmail.com>
2025-11-24 18:26:24 +0900

    FEAT: 비밀번호 변경 DTO

---
+++ src/users/dto/change-password.dto.ts
@@ -0,0 +1,34 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsStrongPassword } from 'class-validator';
+import { MESSAGES } from '../../constants/message.constant';
+
+export class ChangePasswordDto {
+  /**
+   * 기존 비밀번호
+   * @example "Example1!"
+   */
+  @IsNotEmpty({ message: MESSAGES.AUTH.COMMON.PASSWORD.REQUIRED })
+  currentPassword: string;
+  /**
+   * 새 비밀번호
+   * @example "Example1!"
+   */
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    { message: MESSAGES.AUTH.COMMON.PASSWORD.INVALID_FORMAT },
+  )
+  newPassword: string;
+
+  /**
+   * 새 비밀번호 확인
+   * @example "Example1!"
+   */
+  @IsNotEmpty({ message: MESSAGES.AUTH.COMMON.PASSWORD.REQUIRED })
+  newPasswordConfirm: string;
+}
