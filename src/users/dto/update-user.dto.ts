6f68a147b23c8b2e9a84896a2cea4758983a1d45
SW-64 <ettjddns6@gmail.com>
2025-11-24 18:26:43 +0900

    FEAT: 유저 정보 수정 DTO

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,37 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  Length,
+  Matches,
+} from 'class-validator';
+import { MESSAGES } from './../../constants/message.constant';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  /**
+   * 이름
+   * @example "홍길동"
+   */
+  @IsOptional({ message: MESSAGES.AUTH.COMMON.NAME.REQUIRED })
+  @Length(2, 20, { message: MESSAGES.AUTH.COMMON.NAME.INVALID_LENGTH })
+  name: string;
+
+  /**
+   * 이메일
+   * @example "test@example.com"
+   */
+  @IsOptional({ message: MESSAGES.AUTH.COMMON.EMAIL.REQUIRED })
+  @IsEmail({}, { message: MESSAGES.AUTH.COMMON.EMAIL.INVALID_FORMAT })
+  email: string;
+
+  /**
+   * 연락처
+   * @example "01012345678"
+   */
+  @IsOptional({ message: MESSAGES.AUTH.COMMON.PHONE.REQUIRED })
+  @Matches(/^010\d{8}$/, {
+    message: MESSAGES.AUTH.COMMON.PHONE.INVALID_FORMAT,
+  })
+  phone: string;
+}
