b2fbc2565b3f8d6f75dfcde5561b4d35a70249b5
SW-64 <ettjddns6@gmail.com>
2025-11-28 13:03:42 +0900

    FEAT: 자녀 추가(연동 요청) API 구현

--- src/parents/parents.controller.ts
+++ src/parents/parents.controller.ts
@@ -7,8 +7,18 @@ import {
   Param,
   Delete,
   Req,
+  UseGuards,
+  HttpStatus,
 } from '@nestjs/common';
 import { ParentsService } from './parents.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Parent } from './entities/parent.entity';
+import { Role } from '../users/entities/user.entity';
+import { UserInfo } from './../util/decorators/user-info.decorator';
+import { PartialUser } from './../users/interfaces/partial-user.entity';
+import { MESSAGES } from '../constants/message.constant';
 
 @Controller('parents')
 export class ParentsController {
@@ -21,5 +31,20 @@ export class ParentsController {
     const parentId = await this.parentsService.getParentByUserId(userId);
     const students = await this.parentsService.getMyStudents(parentId);
     return students;
+  }
+
+  // 자녀 추가( 연동 요청 )
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.PARENT)
+  @Post('/requests')
+  async createStudentLinkRequest(@UserInfo() user: PartialUser) {
+    const data = await this.parentsService.createStudentLinkRequest(
+      user.userId,
+    );
+    return {
+      statusCode: HttpStatus.OK,
+      message: MESSAGES.PARENTS.STUDENT_LINK.REQUESTED,
+      data: data,
+    };
   }
 }
--- src/parents/parents.service.ts
+++ src/parents/parents.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Parent } from './entities/parent.entity';
 import { Repository } from 'typeorm';
+import { MESSAGES } from 'src/constants/message.constant';
+import { RedisClient } from './../redis/redis.client';
+import { generateCode } from '../util/code.util';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ParentsService {
   constructor(
     @InjectRepository(Parent)
     private readonly parentRepository: Repository<Parent>,
+    private readonly redisClient: RedisClient,
+    private readonly configService: ConfigService,
   ) {}
 
   async getParentByUserId(userId: number) {
@@ -35,5 +41,51 @@ export class ParentsService {
     });
 
     return students;
+  }
+
+  // 자녀 추가(연동 요청)
+  async createStudentLinkRequest(userId: number) {
+    // 0. 유저 유효성검증
+    const parent = await this.parentRepository.findOneBy({ userId });
+    if (!parent) {
+      throw new NotFoundException(MESSAGES.USER.NOT_FOUND);
+    }
+
+    // 1. 부모 기준으로 기존 코드 조회
+    const parentKey = `link:parent:${parent.parentId}`;
+    const oldCode = await this.redisClient.get(parentKey);
+
+    if (oldCode) {
+      await this.redisClient.del(`link:code:${oldCode}`);
+    }
+
+    // 2. 새 코드 생성
+    const code = generateCode();
+
+    // 3. code 기준과 parent 기준 키 둘 다 세팅
+    await this.redisClient.setJson(
+      `link:code:${code}`,
+      { parentId: parent.parentId },
+      this.configService.get<number>('LINK_CODE_EXPIRES_IN'), // TTL(초)
+    );
+
+    await this.redisClient.setString(
+      parentKey,
+      code,
+      this.configService.get<number>('LINK_CODE_EXPIRES_IN'),
+    );
+    await this.saveLinkCode(code, parent.parentId);
+
+    return code;
+  }
+
+  // Redis에 링크 코드 저장
+  async saveLinkCode(code: string, parentId: number) {
+    const expiresIn = this.configService.get<number>('LINK_CODE_EXPIRES_IN');
+    await this.redisClient.setJson(
+      `link:code:${code}`,
+      { parentId },
+      expiresIn,
+    );
   }
 }
