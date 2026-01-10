e31297e026ab544a5c2e0686631451c3ee5aeef2
SW-64 <ettjddns6@gmail.com>
2026-01-10 18:18:21 +0900

    FEAT: 교재 API

---
+++ src/textbook/entities/textbook-chapter.entity.ts
@@ -0,0 +1,37 @@
+// src/textbook/entities/textbook-chapter.entity.ts
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  Index,
+  OneToMany,
+} from 'typeorm';
+import { Textbook } from './textbook.entity';
+import { ProgressChapter } from '../../homework/entities/progress-chapter.entity';
+
+@Entity({ name: 'textbook_chapter' })
+@Index('uq_textbook_unit', ['textbookId', 'largeUnitNo', 'smallUnitNo'], {
+  unique: true,
+})
+export class TextbookChapter {
+  @PrimaryGeneratedColumn({ name: 'textbook_chapter_id' })
+  textbookChapterId: number;
+
+  @Column({ name: 'textbook_id', type: 'int' })
+  textbookId: number;
+
+  @Column({ name: 'large_unit_no', type: 'int' })
+  largeUnitNo: number;
+
+  @Column({ name: 'small_unit_no', type: 'int' })
+  smallUnitNo: number;
+
+  @ManyToOne(() => Textbook, (t) => t.chapters, { onDelete: 'RESTRICT' })
+  @JoinColumn({ name: 'textbook_id', referencedColumnName: 'textbookId' })
+  textbook: Textbook;
+
+  @OneToMany(() => ProgressChapter, (pc) => pc.textbookChapter)
+  progressChapters: ProgressChapter[];
+}
